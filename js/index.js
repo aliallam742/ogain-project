5cea74b6df2806148bbb5632e442d22db2dbf436
aliallam742 <aliallam7472@gmail.com>
2023-12-27 14:25:34 +0200

    this is first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function activMenu(){
+    const dropMenu=document.querySelector("#menu");
+    dropMenu.classList.toggle("activ-menu");
+}
+function actiBare(){
+    const Bare = document.querySelector("#darr");
+    Bare.classList.toggle("activ-bare");
+}
